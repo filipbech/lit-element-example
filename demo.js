d902a98019ecc9f6e9f1301a19c20b12002b6d84
Filip Bech-Larsen <filipbech@filipbech.dk>
2017-10-30 10:58:14 +0100

    use npm version

--- demo.js
+++ demo.js
@@ -1,6 +1,7 @@
 'use strict';
-/** lit-element (https://github.com/kenchris/lit-element) is not yet on npm (there is another thing there with that name) */
-import { html, LitElement } from './lit-element/index.js';
+
+import { html, LitElement } from './node_modules/lit-html-element/lit-element.js';
+import { repeat } from './node_modules/lit-html/lib/repeat.js';
 
 const productApiUrl = 'http://www.json-generator.com/api/json/get/celLKmqymq';
 
